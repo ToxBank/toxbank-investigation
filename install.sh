feeff099cd5d622d62f0232e0e1fe273ff27a3fb
Christoph Helma <helma@in-silico.ch>
2011-12-01 12:19:42 +0000

    isa-validation installation added

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p investigation
+mkdir -p java
+mkdir -p public
+
+cd java
+wget https://github.com/downloads/ISA-tools/ISAvalidator-ISAconverter-BIImanager/ISA-validator-1.4.zip
+unzip ISA-validator-1.4.zip
+cd -
