92693552c909adfd0c348dc483099e5328c374ed
rautenberg <rautenberg@in-silico.ch>
2012-02-08 13:19:25 +0000

    add AA to xls upload test

--- test/xls_upload.rb
+++ test/xls_upload.rb
@@ -44,14 +44,14 @@ class UploadTest < Test::Unit::TestCase
     @isatab_files.each{|f| assert_equal true, File.exists?(File.join(File.expand_path(@tmpdir),f)) }
 
     # get isatab files
-    `curl -H "Accept:text/uri-list" #{uri}`.split("\n").each do |u|
+    `curl -H "Accept:text/uri-list" -H "subjectid:#{@@subjectid}" #{uri}`.split("\n").each do |u|
       puts u
       response = `curl -i -H Accept:text/tab-separated-values -H "subjectid:#{@@subjectid}" #{u}`
       assert_match /200/, response
     end
 
     # delete
-    response = `curl -i -X DELETE #{uri}`
+    response = `curl -i -X DELETE -H "subjectid:#{@@subjectid}" #{uri}`
     assert_match /200/, response
     response = `curl -i -H "Accept:text/uri-list" -H "subjectid:#{@@subjectid}" #{uri}`
     assert_match /404/, response
