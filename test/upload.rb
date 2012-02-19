39c61c3131d5c99a20960a9fc6bebeea8dd59ed5
Christoph Helma <helma@in-silico.ch>
2012-02-19 16:23:57 +0000

    investigation 31 created by 10.0.1.114

--- test/upload.rb
+++ test/upload.rb
@@ -91,7 +91,6 @@ class UploadTest < Test::Unit::TestCase
       puts uri
       zip = File.join @tmpdir,"tmp.zip"
       puts "curl -H 'Accept:application/zip' -H 'subjectid:#{@@subjectid}' #{uri} > #{zip}"
-=begin
       `curl -H "Accept:application/zip" -H "subjectid:#{@@subjectid}" #{uri} > #{zip}`
       #File.open(zip,"w+"){|f| f.puts last_response.body}
       `unzip -o #{zip} -d #{@tmpdir}`
@@ -111,6 +110,7 @@ class UploadTest < Test::Unit::TestCase
         #end
       end
 
+=begin
       # delete
       delete uri, :subjectid => @subjectid
       assert last_response.ok?
