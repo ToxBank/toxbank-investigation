998d393a83f9f826b56908e83a74a3bf0d14cea5
Christoph Helma <helma@in-silico.ch>
2012-03-08 18:54:13 +0000

    error handling moved back to opentox-server

--- application.rb
+++ application.rb
@@ -6,42 +6,6 @@ require File.join(ENV["HOME"],".opentox","config","userpass.rb")
 module OpenTox
   class Application < Service
 
-    # TODO: move to opentox-server
-    error do
-      # TODO: set actor, calling OT::Error with uri parameter does not work
-      error = request.env['sinatra.error']
-      #error.report.actor = "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']}"
-      case request.env['HTTP_ACCEPT']
-      when 'application/rdf+xml'
-        content_type 'application/rdf+xml'
-      when /html/
-        content_type 'text/html'
-      when "text/n3"
-        content_type "text/n3"
-      else
-        content_type "text/turtle"
-      end
-      if error.respond_to? :report
-        case request.env['HTTP_ACCEPT']
-        when 'application/rdf+xml'
-          body = error.report.to_rdfxml
-        when /html/
-          body = error.report.to_yaml
-        when "text/n3"
-          body = error.report.to_ntriples
-        else
-          body = error.report.to_turtle
-        end
-      else
-        content_type "text/plain"
-        body = error.message
-        body += "\n#{error.backtrace}"
-      end
-      code = error.http_code if error.respond_to? :http_code
-      code ||= 500
-      halt code, error.report.to_turtle
-    end
-
     helpers do
 
       def uri
