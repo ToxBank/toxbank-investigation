6d190dfa8f4acd32ab207a653e3b7052fdfd905a
gebele <gebele@in-silico.ch>
2012-05-30 14:54:03 +0200

    enabled zip upload to update existing investigation

--- application.rb
+++ application.rb
@@ -201,6 +201,10 @@ module OpenTox
       bad_request_error "Mime type #{params[:file][:type]} not supported. Please submit data as zip archive (application/zip), Excel file (application/vnd.ms-excel) or as tab separated text (text/tab-separated-values)" unless mime_types.include? params[:file][:type]
       task = OpenTox::Task.create($task[:uri], @subjectid, RDF::DC.description => "#{params[:file][:filename]}: Uploding, validationg and converting to RDF") do
         prepare_upload
+        case params[:file][:type]
+        when 'application/zip'
+          extract_zip
+        end
         # TODO: create initial policy with read permission only for PI 
         isa2rdf
       end
