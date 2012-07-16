81c49ea53a2d6c7bb705033647e49be23495e0d3
rautenberg <rautenberg@in-silico.ch>
2012-07-16 18:45:15 +0200

    add some documentation

--- tbaccount.rb
+++ tbaccount.rb
@@ -15,39 +15,56 @@ module OpenTox
   end
   OpenTox.const_set "TBAccount",c
 
-  #Get rdf reppresentation for a user,organisation or project from the ToxBank service
-  #@example TBAccount
-  #  require "opentox-server"
-  #  User1 = OpenTox::TBAccount.new("http://uri_to_toxbankservice/toxbank/user/U123", subjectid)
-  #  puts User1.ldap_dn #=> "uid=username,ou=people,dc=opentox,dc=org"
-  #  User1.send_policy("http://uri_toprotect/bla/foo") #=> creates new read policy for http://uri_toprotect/bla/foo
+
+
+  # Get rdf representation for a user,organisation or project from the ToxBank service 
+  # @see http://api.toxbank.net/index.php/User ToxBank API User
+  # @see http://api.toxbank.net/index.php/Organisation ToxBank API Organisation
+  # @see http://api.toxbank.net/index.php/Project ToxBank API Project
+  # @see http://api.toxbank.net/index.php/Protocol#Security ToxBank API Security
+  # @example TBAccount
+  #   require "opentox-server"
+  #   User1 = OpenTox::TBAccount.new("http://uri_to_toxbankservice/toxbank/user/U123", subjectid)
+  #   puts User1.ldap_dn #=> "uid=username,ou=people,dc=opentox,dc=org"
+  #   User1.send_policy("http://uri_toprotect/bla/foo") #=> creates new read policy for http://uri_toprotect/bla/foo
   class TBAccount
 
     # Get hasAccount value of a user,organisation or project from ToxBank service
+    # @return [String] username
     def account
       @account ||= get_account
     end
 
-    # returns LDAP Distinguished Name (DN)
+    # Generates LDAP Distinguished Name (DN)
+    # @return [String] LDAP Distinguished Name (DN)
     def ldap_dn
       @uri.match(RDF::TBU.to_s) ? "uid=#{self.account},ou=people,dc=opentox,dc=org" : "cn=#{self.account},ou=groups,dc=opentox,dc=org"
     end
 
-    # returns LDAP type
+    # Get LDAP type - returns 'LDAPUsers' if the TBAccount.uri is a user URI   
+    # @return [String] 'LDAPUsers' or 'LDAPGroups'
     def ldap_type
       @uri.match(RDF::TBU.to_s) ? "LDAPUsers" : "LDAPGroups"
     end
 
+    # GET policy XML 
+    # @param [String,String] URI,Type URI to protect, Access-rights < "all", "readwrite", "read" (default) >
+    # @return [String] policy in XML 
     def get_policy uri, type="read"
       policy(uri, type)
     end
 
     # sends policy to opensso server
+    # @param (see #get_policy) 
     def send_policy uri, type="read"
       OpenTox::Authorization.create_policy(policy(uri, type), @subjectid)
     end
 
-    # Get prefixed account URI e.G.: TBU:U2
+    # Change account URI into RDF prefixed Version e.G.: "http://toxbanktest1.opentox.org:8080/toxbank/user/U2" becomes "TBU:U2"
+    # @example 
+    #   user = OpenTox::TBAccount.new("http://uri_to_toxbankservice/toxbank/user/U2", subjectid)
+    #   puts user.ns_uri #=> "RDF::TBU:U2"
+    # @return [String] prefixed URI of a user/organisation/project
     def ns_uri
       out = "TBU:#{@uri.split('/')[-1]}"  if @uri.match(RDF::TBU.to_s)
       out = "TBO:#{@uri.split('/')[-1]}"  if @uri.match(RDF::TBO.to_s)
@@ -58,6 +75,7 @@ module OpenTox
     private
 
     # Get rdf from user service and returns username
+    # @private 
     def get_account
       begin
         self.metadata[RDF::TB.hasAccount][0].value
@@ -108,10 +126,12 @@ module OpenTox
 
   end
 
+  # ToxBank-investigation specific extension to OpenTox::Authorization in opentox-client 
+  # @see http://rubydoc.info/gems/opentox-client/frames opentox-client documentation
   module Authorization
 
     # Create policy for PI-user (owner of subjectid)
-    # @param [String, String] URI,subjectid URI to create a policy for
+    # @param [String, String] URI,subjectid URI to create a policy for, subjectid
     def self.create_pi_policy uri, subjectid
       user = get_user(subjectid)
       piuri = RestClientWrapper.get("http://toxbanktest1.opentox.org:8080/toxbank/user?username=#{user}", nil, {:Accept => "text/uri-list", :subjectid => subjectid}).sub("\n","")
@@ -119,8 +139,8 @@ module OpenTox
       piaccount.send_policy(uri, "all")
     end
 
-    # resets all investigation policies exept pi policy
-    # @param [String, String, String] URI,LDAPtype,subjectid
+    # Delete all policies for Users or Groups of an investigation except the policy of the PI (Principal Investigator) user. 
+    # @param [String, String, String] URI,LDAPtype,subjectid URI to protect, LDAPUsers or LDAPGroups, subjectid
     def self.reset_policies uri, type, subjectid
       policies = self.list_uri_policies(uri, subjectid)
       user = get_user(subjectid)
