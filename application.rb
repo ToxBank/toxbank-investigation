05768af0f1a2c1cbfb589e5b0f0d2159faa2b9a1
rautenberg <rautenberg@in-silico.ch>
2013-07-15 22:24:18 +0200

    switch to isa2rdf version 0.0.6, set version to v1.0.0beta3

--- application.rb
+++ application.rb
@@ -102,7 +102,7 @@ module OpenTox
       def isa2rdf
         # isa2rdf returns correct exit code but error in task
         # TODO delete dir if task catches error, pass error to block
-        `cd #{File.dirname(__FILE__)}/java && java -jar isa2rdf-cli-0.0.5.jar -d #{tmp} -o #{File.join tmp,nt} -t #{$user_service[:uri]} `#&> #{File.join tmp,'log'}`
+        `cd #{File.dirname(__FILE__)}/java && java -jar isa2rdf-cli-0.0.6.jar -d #{tmp} -o #{File.join tmp,nt} -t #{$user_service[:uri]} `#&> #{File.join tmp,'log'}`
         # rewrite default prefix
         `sed -i 's;http://onto.toxbank.net/isa/tmp/;#{investigation_uri}/;g' #{File.join tmp,nt}`
         investigation_id = `grep "#{investigation_uri}/I[0-9]" #{File.join tmp,nt}|cut -f1 -d ' '`.strip
