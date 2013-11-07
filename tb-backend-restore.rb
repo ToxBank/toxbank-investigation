8e511fb25903fcf50a26082cd7482c09efd27f53
gebele <gebele@in-silico.ch>
2013-11-07 13:54:46 +0000

    give service uri from config file

--- tb-backend-restore.rb
+++ tb-backend-restore.rb
@@ -2,8 +2,7 @@
 # Description: ruby script to restore a destroyed 4store backend from locally stored toxbank-investigation files
 # Date: 07/Nov/2013
 
-SERVICE = "investigation"
-SERVICE_URI = "https://service_uri_here/#{SERVICE}/" # add the service uri
+SERVICE = "investigation" # for service config file
 require File.join '../opentox-client/lib/opentox-client.rb' # maybe adjust the paths here
 require File.join '../opentox-server/lib/opentox-server.rb'
 
@@ -23,7 +22,7 @@ investigations.each_with_index do |inv, idx|
   
   puts "\n========================="
   puts "\nUploading investigation #{idx + 1} with ID #{inv}."
-  uri = SERVICE_URI + inv
+  uri = $investigation[:uri] + inv
   nt = File.join("investigation", inv, inv+".nt")
   OpenTox::Backend::FourStore.put uri, File.read(nt), "application/x-turtle"
   puts "Done."
