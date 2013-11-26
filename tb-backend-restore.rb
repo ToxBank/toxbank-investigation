7957d3a6b3c1d5fdde4e36aaffae4c74e19daa89
gebele <gebele@in-silico.ch>
2013-11-26 09:10:48 +0000

    remove old

--- tb-backend-restore.rb
+++ tb-backend-restore.rb
@@ -17,40 +17,47 @@ end
 # start restore
 puts "\n#{investigations.size} investigations locally stored at the service."
 puts "Start upload to backend at #{$four_store[:uri]}."
+broken_investigations = ""
 
 investigations.each_with_index do |inv, idx|
   
-  puts "\n========================="
-  puts "\nUploading investigation #{idx + 1} with ID #{inv}."
-  uri = $investigation[:uri] + inv
-  nt = File.join("investigation", inv, inv+".nt")
-  OpenTox::Backend::FourStore.put uri, File.read(nt), "application/x-turtle"
-  puts "Done."
-  
-  rdfs = File.join("investigation", inv, "*.rdf")
-  Dir.glob(rdfs).each do |dataset|
-    puts "Upload Dataset #{dataset}."
-    OpenTox::Backend::FourStore.post uri, File.read(dataset), "application/x-turtle"
+  if File.exist?(File.join("investigation", inv, inv+".nt"))
+    puts "\n========================="
+    puts "\nUploading investigation #{idx + 1} with ID #{inv}."
+    uri = $investigation[:uri] + inv
+    nt = File.join("investigation", inv, inv+".nt")
+    OpenTox::Backend::FourStore.put uri, File.read(nt), "application/x-turtle"
     puts "Done."
-  end
-  
-  puts "Upload isSummarySearchable flag."
-  isSS = File.join("investigation", inv, "isSummarySearchable.nt")
-  OpenTox::Backend::FourStore.post uri, File.read(isSS), "application/x-turtle"
-  puts "Done."
-  
-  puts "Upload isPublished flag."
-  isP = File.join("investigation", inv, "isPublished.nt")
-  OpenTox::Backend::FourStore.post uri, File.read(isP), "application/x-turtle"
-  puts "Done."
+    
+    rdfs = File.join("investigation", inv, "*.rdf")
+    Dir.glob(rdfs).each do |dataset|
+      puts "Upload Dataset #{dataset}."
+      OpenTox::Backend::FourStore.post uri, File.read(dataset), "application/x-turtle"
+      puts "Done."
+    end
+    
+    puts "Upload isSummarySearchable flag."
+    isSS = File.join("investigation", inv, "isSummarySearchable.nt")
+    OpenTox::Backend::FourStore.post uri, File.read(isSS), "application/x-turtle"
+    puts "Done."
+    
+    puts "Upload isPublished flag."
+    isP = File.join("investigation", inv, "isPublished.nt")
+    OpenTox::Backend::FourStore.post uri, File.read(isP), "application/x-turtle"
+    puts "Done."
 
-  puts "Update last modified date entry."
-  mod = File.join("investigation", inv, "modified.nt")
-  x = IO.read(mod)
-  # delete all previous entries from OpenTox::Backend::FourStore methods and add date from file
-  OpenTox::Backend::FourStore.update "WITH <#{uri}>
-  DELETE { <#{uri}> <#{RDF::DC.modified}> ?o} WHERE {<#{uri}> <#{RDF::DC.modified}> ?o};
-  INSERT DATA { GRAPH <#{uri}> {<#{uri}> <#{RDF::DC.modified}> #{x.split(">").last.strip}}}"
-  puts "Done.\n"
+    puts "Update last modified date entry."
+    mod = File.join("investigation", inv, "modified.nt")
+    x = IO.read(mod)
+    # delete all previous entries from OpenTox::Backend::FourStore methods and add date from file
+    OpenTox::Backend::FourStore.update "WITH <#{uri}>
+    DELETE { <#{uri}> <#{RDF::DC.modified}> ?o} WHERE {<#{uri}> <#{RDF::DC.modified}> ?o};
+    INSERT DATA { GRAPH <#{uri}> {<#{uri}> <#{RDF::DC.modified}> #{x.split(">").last.strip}}}"
+    puts "Done.\n"
+  else
+    broken_investigations << "#{File.join("investigation", inv)}\n"
+  end
 
 end
+
+puts broken_investigations
