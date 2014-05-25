4a8187fc5b0722892d229e27abcdf2cf08572a58
rautenberg <rautenberg@in-silico.ch>
2014-05-26 00:29:41 +0200

    fix ftp directories

--- helper_isatab.rb
+++ helper_isatab.rb
@@ -120,6 +120,7 @@ module OpenTox
         remove_symlinks
         datafiles = datafiles.collect { |f| [f.gsub(/(ftp:\/\/|)#{URI($investigation[:uri]).host}\//,"")] }
         tolink = (ftpfiles.keys & ( datafiles - Dir.entries(dir).reject{|entry| entry =~ /^\.{1,2}$/}))
+$logger.debug "\nftpfiles:\n#{ftpfiles.inspect} \n\n datafiles: \n #{datafiles}"
         tolink.each do |file|
           `ln -s "#{ftpfiles[file]}" "#{dir}/#{file.gsub("/","_")}"`
           @datahash[file].each do |data_node|
