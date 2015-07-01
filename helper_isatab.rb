bf87cc4e00fbd9dd13fb49f6cd5933ec3fe8ca98
gebele <gebele@in-silico.ch>
2015-07-01 18:06:15 +0200

    fixed line

--- helper_isatab.rb
+++ helper_isatab.rb
@@ -164,7 +164,6 @@ module OpenTox
                 $logger.debug "chunkfiles:\t#{chunkfiles}"
                 
                 # append datasets to investigation graph
-                chunkfiles.sort{|a,b| a <=> b}.each do |dataset|
                 datafiles.each do |dataset|
                   OpenTox::Backend::FourStore.post investigation_uri, File.read(dataset), "application/x-turtle"
                   sleep 10 # time it takes to import and reindex
